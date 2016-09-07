b08d5868d40b048b29666183652a961eec022bec
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-09-07 20:07:42 -0300

    executions fix

--- datastores/database/executions.go
+++ datastores/database/executions.go
@@ -15,10 +15,15 @@ func NewExecstore(db *sqlx.DB) *Execstore {
 	return &Execstore{db}
 }
 
+const executionsStmQuery = `
+	UPDATE tokens
+	SET next = now() + interval '1 day', updated_at = now()
+	WHERE next <= now()
+	RETURNING user_id, token
+`
+
 // Executions get the executions that should be made
 func (db *Execstore) Executions() ([]datastores.Execution, error) {
 	var executions []datastores.Execution
-	return executions, db.Select(
-		&executions, "SELECT user_id, token FROM tokens WHERE next <= now()",
-	)
+	return executions, db.Select(&executions, executionsStmQuery)
 }
