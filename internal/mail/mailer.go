0953cfd1688a9b2c25c86f650ea46c77b924aaf1
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-09-07 22:36:28 -0300

    move email templates to html files

--- internal/mail/mailer.go
+++ internal/mail/mailer.go
@@ -11,87 +11,19 @@ import (
 	"github.com/sendgrid/sendgrid-go/helpers/mail"
 )
 
-const welcomeTemplate = `
-<h2>Hi @{{.Login}}!</h2>
-
-<p>
-	You have <strong>{{.Followers}}</strong> followers.
-	We will watch for more changes and let you know!
-</p>
-
-<p style="color: #333">
-	Sincerely,<br/>
-	The Watchub Team
-<p>
-`
-
-const changesTemplate = `
-<h2>Hi @{{.Login}}!</h2>
-
-<p>
-	Hey, here what changed in your account recently:
-</p>
-
-{{if .NewFollowers }}
-	<p>
-		<h3>People who are now <strong>following</strong> you:</h3>
-		<ul>
-			{{range $index, $element := .NewFollowers}}
-			<li>
-				<a href="http://github.com/{{.}}">{{.}}</a>
-			</li>
-			{{end}}
-		</ul>
-	</p>
-{{end}}
-
-{{if .Unfollowers }}
-	<p>
-		<h3>People who <strong>unfollowed</strong> you:</h3>
-		<ul>
-			{{range $index, $element := .Unfollowers}}
-			<li>
-				<a href="http://github.com/{{.}}">{{.}}</a>
-			</li>
-			{{end}}
-		</ul>
-	</p>
-{{end}}
-
-<p>
-	You now have <strong>{{.Followers}}</strong> followers.
-	We will watch for more changes and let you know!
-</p>
-
-<p style="color: #333">
-	Sincerely,<br/>
-	The Watchub Team
-<p>
-`
-
 // Mailer
 type Mailer struct {
-	key         string
-	from        string
-	welcomeTmpl *template.Template
-	changesTmpl *template.Template
+	key       string
+	from      string
+	templates *template.Template
 }
 
 // New mailer
 func New(config config.Config) *Mailer {
-	welcomeTmpl, err := template.New("?").Parse(welcomeTemplate)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	changesTmpl, err := template.New("?").Parse(changesTemplate)
-	if err != nil {
-		log.Fatalln(err)
-	}
 	return &Mailer{
-		key:         config.SendgridAPIKey,
-		from:        "caarlos0@gmail.com",
-		welcomeTmpl: welcomeTmpl,
-		changesTmpl: changesTmpl,
+		key:       config.SendgridAPIKey,
+		from:      "caarlos0@gmail.com",
+		templates: template.Must(template.ParseGlob("static/mail/*.html")),
 	}
 }
 
@@ -134,7 +66,7 @@ func (mailer *Mailer) send(name, email, content, subject string) {
 func (mailer *Mailer) SendChanges(data ChangesData) {
 	subject := "Your report from Watchub!"
 	var mailContent bytes.Buffer
-	err := mailer.changesTmpl.Execute(&mailContent, data)
+	err := mailer.templates.ExecuteTemplate(&mailContent, "welcome.html", data)
 	if err != nil {
 		log.Println("Failed to mail", data, ".", err)
 	}
@@ -145,7 +77,7 @@ func (mailer *Mailer) SendChanges(data ChangesData) {
 func (mailer *Mailer) SendWelcome(data WelcomeData) {
 	subject := "Welcome to Watchub!"
 	var mailContent bytes.Buffer
-	err := mailer.welcomeTmpl.Execute(&mailContent, data)
+	err := mailer.templates.ExecuteTemplate(&mailContent, "changes.html", data)
 	if err != nil {
 		log.Println("Failed to mail", data, ".", err)
 	}
