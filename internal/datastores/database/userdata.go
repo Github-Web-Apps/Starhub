d096ebe414d2e89577315a1e50f61008079d8522
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-09-08 21:04:31 -0300

    wip stars

--- internal/datastores/database/userdata.go
+++ internal/datastores/database/userdata.go
@@ -1,8 +1,9 @@
 package database
 
 import (
-	"database/sql"
+	"encoding/json"
 
+	"github.com/caarlos0/watchub/internal/datastores"
 	"github.com/jmoiron/sqlx"
 	"github.com/lib/pq"
 )
@@ -19,21 +20,11 @@ func NewUserdatastore(db *sqlx.DB) *Userdatastore {
 
 // GetFollowers of a given userID
 func (db *Userdatastore) GetFollowers(userID int64) ([]string, error) {
-	var logins []sql.NullString
-	var result []string
-	err := db.QueryRow(
+	var logins []string
+	return logins, db.QueryRow(
 		"SELECT followers FROM tokens WHERE user_id = $1",
 		userID,
 	).Scan(pq.Array(&logins))
-	if err != nil {
-		return result, err
-	}
-	for _, id := range logins {
-		if id.Valid {
-			result = append(result, id.String)
-		}
-	}
-	return result, nil
 }
 
 // SaveFollowers for a given userID
@@ -44,4 +35,20 @@ func (db *Userdatastore) SaveFollowers(userID int64, followers []string) error {
 		pq.Array(followers),
 	)
 	return err
+}
+
+func (db *Userdatastore) GetStars(userID int64) ([]datastores.Stars, error) {
+	var result []datastores.Stars
+	var stars json.RawMessage
+	if err := db.QueryRow(
+		"SELECT stars FROM tokens WHERE user_id = $1",
+		userID,
+	).Scan(&stars); err != nil {
+		return result, err
+	}
+	return result, json.Unmarshal(stars, &result)
+}
+
+func (db *Userdatastore) SaveStars(userID int64, stars []datastores.Stars) error {
+	return nil
 }
--- internal/datastores/userdata.go
+++ internal/datastores/userdata.go
@@ -3,4 +3,12 @@ package datastores
 type Userdatastore interface {
 	GetFollowers(userID int64) ([]string, error)
 	SaveFollowers(userID int64, followers []string) error
+	GetStars(userID int64) ([]Stars, error)
+	SaveStars(userID int64, stars []Stars) error
+}
+
+type Stars struct {
+	RepoID     int64    `json:"repo_id"`
+	RepoName   string   `json:"repo_name"`
+	Stargazers []string `json:"stargazers"`
 }
