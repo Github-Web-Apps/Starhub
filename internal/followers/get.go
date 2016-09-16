e2e4398906d144a93b806a62ec5088f40e5c2cb4
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-09-15 23:18:48 -0300

    added tests to followers.Get, improved performance and added rate limit
    refs #13
    
    closes #4

--- internal/followers/get.go
+++ internal/followers/get.go
@@ -1,22 +1,38 @@
 package followers
 
-import "github.com/google/go-github/github"
+import (
+	"time"
+
+	"github.com/google/go-github/github"
+)
 
 // Get the list of followers of a given user
-func Get(client *github.Client) ([]*github.User, error) {
-	opt := &github.ListOptions{PerPage: 10}
+func Get(client *github.Client) (result []*github.User, err error) {
+	opt := &github.ListOptions{PerPage: 30}
 
-	var allFollowers []*github.User
 	for {
-		followers, resp, err := client.Users.ListFollowers("", opt)
+		followers, nextPage, err := getPage(opt, client)
 		if err != nil {
-			return allFollowers, err
+			return result, err
 		}
-		allFollowers = append(allFollowers, followers...)
-		if resp.NextPage == 0 {
+		result = append(result, followers...)
+		if opt.Page = nextPage; nextPage == 0 {
 			break
 		}
-		opt.Page = resp.NextPage
 	}
-	return allFollowers, nil
+	return result, nil
+}
+
+func getPage(
+	opt *github.ListOptions, client *github.Client,
+) (followers []*github.User, nextPage int, err error) {
+	followers, resp, err := client.Users.ListFollowers("", opt)
+	if err != nil {
+		if _, ok := err.(*github.RateLimitError); ok {
+			time.Sleep(time.Duration(15) * time.Second)
+			return getPage(opt, client)
+		}
+		return followers, 0, err
+	}
+	return followers, resp.NextPage, err
 }
