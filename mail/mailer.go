955387013e9f9e9b931f3d28a8dcb38d5c2e7df9
Carlos Alexandro Becker <caarlos0@gmail.com>
2017-11-11 15:06:52 -0200

    new email layout

--- mail/mailer.go
+++ mail/mailer.go
@@ -2,88 +2,129 @@ package mail
 
 import (
 	"bytes"
+	"fmt"
 	"html/template"
+	"strings"
+	"time"
 
 	"github.com/apex/log"
 	"github.com/caarlos0/watchub/config"
 	"github.com/caarlos0/watchub/shared/dto"
+	"github.com/matcornic/hermes"
 	sendgrid "github.com/sendgrid/sendgrid-go"
 	"github.com/sendgrid/sendgrid-go/helpers/mail"
 )
 
-// Mailer service
-type Mailer struct {
-	key       string
-	from      string
-	templates *template.Template
+var emailConfig = hermes.Hermes{
+	Product: hermes.Product{
+		Name:      "Watchub",
+		Link:      "http://watchub.pw",
+		Logo:      "https://raw.githubusercontent.com/caarlos0/watchub/master/static/apple-touch-icon-144x144.png",
+		Copyright: "Copyright © 2016-2017 Watchub.",
+	},
+	Theme: new(hermes.Flat),
 }
 
-// New mailer
-func New(config config.Config) *Mailer {
-	return &Mailer{
-		key:  config.SendgridAPIKey,
-		from: "noreply@watchub.pw",
+var welcomeIntro = []string{
+	"Welcome to Watchub!",
+	"We're very excited to have you on board.",
+}
+
+var changesIntro = []string{
+	"Here is what changed in your account recently:",
+}
+
+func New(config config.Config) *MailSvc {
+	return &MailSvc{
+		hermes:  emailConfig,
+		changes: template.Must(template.ParseFiles("static/mail/changes.md")),
+		welcome: template.Must(template.ParseFiles("static/mail/welcome.md")),
+		config:  config,
 	}
 }
 
-// SendChanges report to an existing user
-func (mailer *Mailer) SendChanges(data dto.ChangesEmailData) {
-	mailer.send(
-		data.Login,
-		data.Email,
-		"Your report from Watchub!",
-		"changes",
-		data,
-	)
+type MailSvc struct {
+	hermes  hermes.Hermes
+	config  config.Config
+	changes *template.Template
+	welcome *template.Template
 }
 
-// SendWelcome email to a new user
-func (mailer *Mailer) SendWelcome(data dto.WelcomeEmailData) {
-	mailer.send(
-		data.Login,
-		data.Email,
-		"Welcome to Watchub!",
-		"welcome",
-		data,
-	)
-}
-
-func (mailer *Mailer) send(name, email, subject, templateName string, data interface{}) {
-	var log = log.WithField("email", email).WithField("template", templateName)
-	var from = mail.NewEmail("Watchub", mailer.from)
-	var to = mail.NewEmail(name, email)
-	var content, err = executeTemplate(templateName, data)
+func (s *MailSvc) SendWelcome(data dto.WelcomeEmailData) {
+	html, err := s.generate(data.Login, data, s.welcome, welcomeIntro)
 	if err != nil {
-		log.WithError(err).Error("failed execute email template")
+		log.WithError(err).Error("failed to generate welcome email")
 		return
 	}
+	s.send(data.Login, data.Email, "Welcome to Watchub!", html)
+}
+
+func (s *MailSvc) SendChanges(data dto.ChangesEmailData) {
+	html, err := s.generate(data.Login, data, s.changes, changesIntro)
+	if err != nil {
+		log.WithError(err).Error("failed to generate changes email")
+		return
+	}
+	s.send(data.Login, data.Email, "Your report from Watchub!", html)
+}
+
+func (s *MailSvc) generate(login string, data interface{}, tmpl *template.Template, intros []string) (string, error) {
+	var wr bytes.Buffer
+	if err := tmpl.Execute(&wr, data); err != nil {
+		return "", err
+	}
+	return s.hermes.GenerateHTML(
+		hermes.Email{
+			Body: hermes.Body{
+				Name:   login,
+				Intros: intros,
+				FreeMarkdown: hermes.Markdown(
+					strings.Join(
+						[]string{
+							wr.String(),
+							"\n\n",
+							"We will continue to watch for changes and let you know!",
+							"\n\n---\n\n",
+							"<small>",
+							`Liking our service? Maybe you'll consider [make a donation](http://watchub.pw/donate).`,
+							fmt.Sprintf(
+								`You might also want to change [your settings](%s).`,
+								"https://github.com/settings/connections/applications/"+s.config.ClientID,
+							),
+							"</small>",
+						},
+						" ",
+					),
+				),
+			},
+		},
+	)
+}
+
+func (s *MailSvc) send(name, email, subject, html string) {
+	var log = log.WithField("email", email)
+	var from = mail.NewEmail("Watchub", "noreply@watchub.pw")
+	var to = mail.NewEmail(name, email)
 	var request = sendgrid.GetRequest(
-		mailer.key,
+		s.config.SendgridAPIKey,
 		"/v3/mail/send",
 		"https://api.sendgrid.com",
 	)
+	// prevent grouping in gmail
+	request.Headers["X-Entity-Ref-ID"] = "watchub-" + time.Now().String()
 	request.Method = "POST"
 	request.Body = mail.GetRequestBody(
 		mail.NewV3MailInit(
 			from,
 			subject,
 			to,
-			mail.NewContent("text/html", content.String()),
+			mail.NewContent("text/html", html),
 		),
 	)
 	resp, err := sendgrid.API(request)
 	if err != nil {
-		log.WithError(err).Error("failed to send email")
+		log.WithError(err).WithField("resp", resp).Error("failed to send email")
 		return
 	}
 	log.WithField("status", resp.StatusCode).Info("email request posted")
-}
-
-func executeTemplate(name string, data interface{}) (bytes.Buffer, error) {
-	var content bytes.Buffer
-	templates, _ := template.ParseFiles(
-		"static/mail/layout.html",
-		"static/mail/"+name+".html",
-	)
-	return content, templates.ExecuteTemplate(&content, "layout", data)
 }
